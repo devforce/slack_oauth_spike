0e9c96db51917eca9847fd54187b10ed87a276c9
Luyang Tang <luyang.tang@luyangtang-ltm.internal.salesforce.com>
2021-02-22 09:31:58 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config({path: __dirname + '/.env'})
+
+const { App } = require('@slack/bolt');
+
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+});
+
+/* Add functionality here */
+
+(async () => {
+  // Start the app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
